7a39f899dc893a2ca5e4f4a406b99d9e538b04aa
Sikha Das <sikha.das@snowflake.com>
2025-01-09 16:47:03 -0500

    Update setup.sql

--- scripts/setup.sql
+++ scripts/setup.sql
@@ -4,7 +4,7 @@ SELECT $USERNAME;
 
 -- Using ACCOUNTADMIN, create a new role for this exercise and grant to applicable users
 CREATE OR REPLACE ROLE EMBEDDING_MODEL_HOL_USER;
-GRANT ROLE EMBEDDING_MODEL_QUICKSTART_USER to USER identifier($USERNAME);
+GRANT ROLE EMBEDDING_MODEL_HOL_USER to USER identifier($USERNAME);
 
 -- Next create a new database and schema,
 CREATE DATABASE IF NOT EXISTS EMBEDDING_MODEL_HOL_DB;
